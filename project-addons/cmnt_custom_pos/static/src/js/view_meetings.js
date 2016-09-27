221ba7bd9368c71dc5adad8bcdf5b6fcbd2589b6
javierjcf <javierjcf@gmail.com>
2016-09-27 19:37:10 +0200

    [ADD]cmnt_custom_pos: Prototipo vista tabla

--- project-addons/cmnt_custom_pos/static/src/js/view_meetings.js
+++ project-addons/cmnt_custom_pos/static/src/js/view_meetings.js
@@ -2,32 +2,43 @@ function openerp_custom_view_meetings(instance, module){
     var QWeb = instance.web.qweb;
 	var _t = instance.web._t;
 
-   
+    module.ViewMeetingScreenWidget = module.ScreenWidget.extend({
+        template: 'ViewMeetingScreenWidget',
+
+        show_leftpane: false,
+        previous_screen: 'products',
+
+        renderElement: function(){
+            var self = this;
+            this._super();
+
+            // Funcionality of back button
+            this.$('.back').click(function(){
+                self.pos_widget.screen_selector.set_current_screen(self.previous_screen);
+            });
+        },
+
+    });
 
     module.PosWidget.include({
         build_widgets: function(){
             var self = this;
             this._super();
 
-            // if(this.pos.config.iface_splitbill){
-            //     this.splitbill_screen = new module.SplitbillScreenWidget(this,{});
-            //     this.splitbill_screen.appendTo(this.$('.screens'));
-            //     this.screen_selector.add_screen('splitbill',this.splitbill_screen);
-
-            //     var splitbill = $(QWeb.render('SplitbillButton'));
-
-            //     splitbill.click(function(){
-            //         if(self.pos.get('selectedOrder').get('orderLines').models.length > 0){
-            //             self.pos_widget.screen_selector.set_current_screen('splitbill');
-            //         }
-            //     });
-                
-            //     splitbill.appendTo(this.$('.control-buttons'));
-            //     this.$('.control-buttons').removeClass('oe_hidden');
-            // }
+            // Add View Meetings Buttom
             var view_meeting = $(QWeb.render('ViewMettingButton'));
             view_meeting.appendTo(this.$('.control-buttons'));
             this.$('.control-buttons').removeClass('oe_hidden');
+
+            // Add Meetings Screen
+            this.view_meeting_screen = new module.ViewMeetingScreenWidget(this, {});
+            this.view_meeting_screen.appendTo(this.$('.screens'));
+            this.screen_selector.add_screen('view_meeting', this.view_meeting_screen);
+
+            // Link screen to click in Button
+            view_meeting.click(function(){
+                self.pos_widget.screen_selector.set_current_screen('view_meeting');
+            });
             
         },
     });
